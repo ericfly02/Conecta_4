5bc609c37912cc3c9f710b033292889a3a5c25f2
EricGd <speedyand@gmail.com>
2022-11-20 21:24:38 +0100

    done

--- c4_the_game/src/edu/epsevg/prop/lab/c4/Juga2.java
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/Juga2.java
@@ -95,11 +95,11 @@ public class Juga2 extends javax.swing.JFrame {
         //</editor-fold>
 
         // Definiu al vostre gust els jugadors a enfrontar.
-        Jugador p1 = new MinMax();
+        Jugador p2 = new MinMax();
         //Jugador p2 = new Manual();
 
-        Jugador p2 = new Profe(8, false);
-        //Jugador p2 = new Manual();
+        //Jugador p1 = new Profe(8, false);
+        Jugador p1 = new Aleatori();
 
         boolean autoMode = true;
         final Juga2 j = new Juga2(p1, p2, autoMode);
--- c4_the_game/src/edu/epsevg/prop/lab/c4/MinMax.java
+++ c4_the_game/src/edu/epsevg/prop/lab/c4/MinMax.java
@@ -48,195 +48,190 @@ public class MinMax implements Jugador, IAuto{
   que tingui l'altre jugador. Així, si la heuristica es 0, vol dir que el tauler esta equilibrat, si es major que 0 vol dir que tenim mes fitxes que ell (en possible posició guanyadora)
   i si es menor que 0 vol dir que ell te mes fitxes (en possible posició guanyadora) que nosaltres.
   */
-
-  public int heuristica(Tauler t){
-    // La variable maxima_puntuacio ens dira, en cas que no hi hagi ningun moviment guanyador, quin es el maxim de punts que podem aconseguir
+  public int vertical(Tauler t, int columna, int fila, int color, int grup){
+    int contador = 0;
     
-    int mida = t.getMida();
-    int max_puntuacio = 0;
-    int min_puntuacio = 0;
-    
-    for (int i = 0; i < mida; i++){
-      for (int j = 0; j < mida; j++){
-        // Analitzem si hi ha moviment guanyador per a nosaltres (color 1)
-        if (t.getColor(i, j) == 1){
-          //4 en ratlla horitzontal
-          if (j < mida - 3){
-            if (t.getColor(i, j + 1) == 1 && t.getColor(i, j + 2) == 1 && t.getColor(i, j + 3) == 1){
-              return Integer.MAX_VALUE;
-            }
-          }
-          //4 en ratlla vertical
-          if (i < mida - 3){
-            if (t.getColor(i + 1, j) == 1 && t.getColor(i + 2, j) == 1 && t.getColor(i + 3, j) == 1){
-              return Integer.MAX_VALUE;
-            }
-          }
-          //4 en ratlla diagonal dreta
-          if (i < mida - 3 && j < mida - 3){
-            if (t.getColor(i + 1, j + 1) == 1 && t.getColor(i + 2, j + 2) == 1 && t.getColor(i + 3, j + 3) == 1){
-              return Integer.MAX_VALUE;
-            }
-          }
-          //4 en ratlla diagonal esquerra
-          if (i < mida - 3 && j > 2){
-            if (t.getColor(i + 1, j - 1) == 1 && t.getColor(i + 2, j - 2) == 1 && t.getColor(i + 3, j - 3) == 1){
-              return Integer.MAX_VALUE;
-            }
-          }
-          
-          //Mirem si hi ha 3 fitxes seguides en vertical
-          if (i < mida - 2){
-            if (t.getColor(i + 1, j) == -1 && t.getColor(i + 2, j) == -1){
-              max_puntuacio +=  100;
-           }
-          }
-          //Mirem si hi ha 3 fitxes seguides en horitzontal
-          if (j < mida - 2){
-            if (t.getColor(i, j + 1) == -1 && t.getColor(i, j + 2) == -1){
-              max_puntuacio += 100;
-            }
-          }
-          //Mirem si hi ha 3 fitxes seguides en diagonal dreta  
-          if (i < mida - 2 && j < mida - 2){
-            if (t.getColor(i + 1, j + 1) == -1 && t.getColor(i + 2, j + 2) == -1){
-              max_puntuacio += 100;
-            }
-          }
-          // Mirem si hi ha 3 fitxes seguides en diagonal esquerra
-          if (i < mida - 2 && j > 1){
-            if (t.getColor(i + 1, j - 1) == -1 && t.getColor(i + 2, j - 2) == -1){
-              max_puntuacio += 100;
-            }
-          }
-
-          //Mirem si hi ha 2 fitxes seguides en vertical
-          if (i < mida - 1){
-            if (t.getColor(i + 1, j) == -1){
-              max_puntuacio += 20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en horitzontal
-          if (j < mida - 1){
-            if (t.getColor(i, j + 1) == -1){
-             max_puntuacio += 20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en diagonal dreta
-          if (i < mida - 1 && j < mida - 1){
-            if (t.getColor(i + 1, j + 1) == -1){
-              max_puntuacio += 20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en diagonal esquerra
-          if (i < mida - 1 && j > 0){
-            if (t.getColor(i + 1, j - 1) == -1){
-              max_puntuacio += 20;
-            }
-          }
-
-          // Si la puntuacio es mes gran que la maxima puntuacio, la maxima puntuacio sera la puntuacio actual
-          //if (max_puntuacio < puntuacio1) max_puntuacio = puntuacio1;
-          
-        }
-        //-----------------------------------------------------------------
-        // Analitzem si hi ha moviment guanyador per al contrari (color -1)
-        else if (t.getColor(i, j) == -1){
-          //Mirem si hi ha 4 en ratlla horitzontal
-          if (j < mida - 3){
-            if (t.getColor(i, j + 1) == -1 && t.getColor(i, j + 2) == -1 && t.getColor(i, j + 3) == -1){
-              return Integer.MIN_VALUE;
-            }
-          }
-          //Mirem si hi ha 4 en ratlla vertical
-          if (i < mida - 3){
-            if (t.getColor(i + 1, j) == -1 && t.getColor(i + 2, j) == -1 && t.getColor(i + 3, j) == -1){
-              return Integer.MIN_VALUE;
-            }
-          }
-          //Mirem si hi ha 4 en ratlla diagonal dreta
-          if (i < mida - 3 && j < mida - 3){
-            if (t.getColor(i + 1, j + 1) == -1 && t.getColor(i + 2, j + 2) == -1 && t.getColor(i + 3, j + 3) == -1){
-              return Integer.MIN_VALUE;
-            }
-          }
-          //Mirem si hi ha 4 en ratlla diagonal esquerra
-          if (i < mida - 3 && j > 2){
-            if (t.getColor(i + 1, j - 1) == -1 && t.getColor(i + 2, j - 2) == -1 && t.getColor(i + 3, j - 3) == -1){
-              return Integer.MIN_VALUE;
-            }
-          }
-          
-          //Mirem si hi ha 3 fitxes seguides en vertical
-          if (i < mida - 2){
-            if (t.getColor(i + 1, j) == -1 && t.getColor(i + 2, j) == -1){
-              min_puntuacio += -100;
-            }
-          }
-          //Mirem si hi ha 3 fitxes seguides en horitzontal
-          if (j < mida - 2){
-            if (t.getColor(i, j + 1) == -1 && t.getColor(i, j + 2) == -1){
-              min_puntuacio += -100;
-            }
-          }
-          //Mirem si hi ha 3 fitxes seguides en diagonal dreata
-          if (i < mida - 2 && j < mida - 2){
-            if (t.getColor(i + 1, j + 1) == -1 && t.getColor(i + 2, j + 2) == -1){
-              min_puntuacio += -100;
-            }
-          }
-          //Mirem si hi ha 3 fitxes seguides en diagonal esquerra
-          if (i < mida - 2 && j > 1){
-            if (t.getColor(i + 1, j - 1) == -1 && t.getColor(i + 2, j - 2) == -1){
-              min_puntuacio += -100;
-            }
-          }
-
-          //Mirem si hi ha 2 fitxes seguides en vertical
-          if (i < mida - 1){
-            if (t.getColor(i + 1, j) == -1){
-              min_puntuacio += -20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en horitzontal
-          if (j < mida - 1){
-            if (t.getColor(i, j + 1) == -1){
-              min_puntuacio += -20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en diagonal dreta
-          if (i < mida - 1 && j < mida - 1){
-            if (t.getColor(i + 1, j + 1) == -1){
-              min_puntuacio += -20;
-            }
-          }
-          //Mirem si hi ha 2 fitxes seguides en diagonal esquerra
-          if (i < mida - 1 && j > 0){
-            if (t.getColor(i + 1, j - 1) == -1){
-              min_puntuacio += -20;
-            }
-          }
-        }        
+    for(int i = fila; i < t.getMida(); i++){    
+      if(t.getColor(i, columna) == t.getColor(fila, columna)){
+        contador++;
+      }
+      else{
+        break;
       }
     }
     
-    return max_puntuacio+min_puntuacio;
+    if (contador >= grup){
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  public int horitzontal(Tauler t, int columna, int fila, int color, int grup){
+    int contador = 0;
+    
+    for(int i = columna; i < t.getMida(); i++){
+      if(t.getColor(fila, i) == t.getColor(fila, columna)){
+        contador++;
+      }
+      else{
+        break;
+      }
+    }
+    
+    if (contador >= grup){
+      return 1;
+    }
+    else
+    {
+      return 0;
+    }
+  }
+
+  
+  public int diagonal(Tauler t, int columna, int fila, int color, int grup){
+    int contador = 0;
+    int total = 0;
+
+    int j = columna;
+
+    //Mirem la diagonal ascendent
+    for (int i = fila; i < t.getMida(); i++)
+    {
+      if (j > t.getMida()-1)
+      {
+        break;
+      }
+      else if (t.getColor(fila, columna) == t.getColor(i, j))
+      {
+        contador += 1;
+      }
+      else break;
+
+      j += 1;
+    }
+    if (contador >= grup) total += 1;
+
+    //Mirem la diagonal descendent
+    contador = 0;
+    j = columna;
+
+    for (int i = fila; i > -1; i--)
+    {
+      if (j > t.getMida()-1)
+      {
+        break;
+      }
+      else if (t.getColor(fila, columna) == t.getColor(i, j))
+      {
+        contador += 1;
+      }
+      else break;
+
+      j += 1;
+    }
+    if (contador >= grup) total += 1;
+    return total;
+  }
+  
+  public int heuristica(Tauler t, int color){
+    // La variable maxima_puntuacio ens dira, en cas que no hi hagi ningun moviment guanyador, quin es el maxim de punts que podem aconseguir
+    
+    int color_oponent;
+    if (color == 1)
+    {
+      color_oponent = -1;
+    }
+    else color_oponent = 1;
+
+    int fitxes_meves_4 = 0;
+    int fitxes_meves_3 = 0;
+    int fitxes_meves_2 = 0; 
+    int fitxes_oponent_4 = 0; 
+    int fitxes_oponent_3  = 0;
+    int fitxes_oponent_2 = 0; 
+
+    for (int i = 0; i < t.getMida(); i++)
+    {
+      for (int j = 0; j < t.getMida(); j++)
+      {
+        if (t.getColor(i, j) == color)
+        {
+          //Mirem quants grups de 4 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_meves_4 += horitzontal(t, i, j, color, 4);
+          fitxes_meves_4 += vertical(t, i, j, color, 4);
+          fitxes_meves_4 += diagonal(t, i, j, color, 4);
+
+          //Mirem quants grups de 3 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_meves_3 += horitzontal(t, i, j, color, 3);
+          fitxes_meves_3 += vertical(t, i, j, color, 3);
+          fitxes_meves_3 += diagonal(t, i, j, color, 3);
+          
+          //Mirem quants grups de 2 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_meves_2 += horitzontal(t, i, j, color, 2);
+          fitxes_meves_2 += vertical(t, i, j, color, 2);
+          fitxes_meves_2 += diagonal(t, i, j, color, 2);
+        }
+        else if (t.getColor(i, j) == color_oponent)
+        {
+          //Mirem quants grups de 4 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_oponent_4 += horitzontal(t, i, j, color_oponent, 4);
+          fitxes_oponent_4 += vertical(t, i, j, color_oponent, 4);
+          fitxes_oponent_4 += diagonal(t, i, j, color_oponent, 4);
+
+          //Mirem quants grups de 3 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_oponent_3 += horitzontal(t, i, j, color_oponent, 3);
+          fitxes_oponent_3 += vertical(t, i, j, color_oponent, 3);
+          fitxes_oponent_3 += diagonal(t, i, j, color_oponent, 3);
+
+          //Mirem quants grups de 2 fitxes hi ha, en horitzontal, en vertical i en diagonal
+          fitxes_oponent_2 += horitzontal(t, i, j, color_oponent, 2);
+          fitxes_oponent_2 += vertical(t, i, j, color_oponent, 2);
+          fitxes_oponent_2 += diagonal(t, i, j, color_oponent, 2);
+        }
+      }
+    }
+    
+    // Retornem la puntuacio de la heuristica la qual es calcula com la suma de tots els grups de 4, 3 i 2 fitxes que tenim, restant els grups de 4, 3 i 2 fitxes que te l'oponent
+    // Aixo es fa perque si tenim un grup de 4 fitxes i l'oponent no en te cap, aixo ens dona una puntuacio de 1000, i si l'oponent te un grup de 4 fitxes i nosaltres no en tenim cap, aixo ens dona una puntuacio de -1000
+    // Es a dir, cada grup de fitxes, es multiplica per un pes que depen de la seva mida, i aixo ens dona una puntuacio que ens indica si tenim una posicio millor que l'oponent o no
+    int fitxes_meves = fitxes_meves_4*1000 + fitxes_meves_3*250 + fitxes_meves_2*2;
+    int fitxes_oponent = fitxes_oponent_4*1000 + fitxes_oponent_3*250 + fitxes_oponent_2*2;
+    return fitxes_meves - fitxes_oponent;
   }
 
   
   // Creació de la funcio MINIMAX
-  public int[] minimax(Tauler t, int color, int profunditat, int alpha, int beta){
+  public int[] minimax(Tauler t, int color, int profunditat, int alpha, int beta, int columna){
     // Si el joc s'ha acabat o la profunditat es 0, retornem la heuristica
-    int a = heuristica(t);
     
-    if (!t.espotmoure() || profunditat == 0 || a >= Integer.MAX_VALUE || a <= Integer.MIN_VALUE){
-      //System.out.println("FINAL:" + a);
-      int[] aux = new int[]{-1,a};
-      return aux;
+    
+      
+    if (t.solucio(columna, -1))
+    {
+      return new int[]{0, 9999999};
     }
+    else if (t.solucio(columna, 1))
+    {
+      return new int[]{0, -9999999};
+    }
+    else if (!t.espotmoure())
+    {
+      return new int[]{0,0};
+    }
+    else if (profunditat == 0)
+    {
+      return new int[]{0,heuristica(t, color)};
+    }
+    
+
+
     // Si es el torn de la maquina, busquem minimitzar
     if (color == -1){
-      int[] max = new int[]{-1,0};
+      
+      int[] max = new int[]{(int)(t.getMida() * Math.random()),-9999999};
       for (int i = 0; i < t.getMida(); i++){
         if (t.movpossible(i)){
           // Creem un nou tauler per a realitzar el moviment
@@ -244,15 +239,15 @@ public class MinMax implements Jugador, IAuto{
           copia.afegeix(i, color);
 
           // Recursivitat: cridem a la funcio amb el nou tauler, el color contrari i la profunditat disminuida, i actualitzem el valor de beta
-          int[] val = minimax(copia, 1, profunditat - 1, alpha, beta);
+          int[] proxim = minimax(copia, 1, profunditat - 1, alpha, beta, i);
 
-          if(val[0] == -1 || val[1] > max[1]){
+          if(proxim[1] > max[1]){
             max[0] = i;
-            max[1] = heuristica(t);
-            alpha = heuristica(t);
+            max[1] = proxim[1];
           }
+          alpha = Math.max(alpha, proxim[1]);
           // Si el valor de beta es menor o igual que alpha, retornem el valor de beta
-          if (beta <= alpha) return max;
+          if (beta <= alpha) break;
         }
       }
       return max;
@@ -260,7 +255,7 @@ public class MinMax implements Jugador, IAuto{
 
     // Si es el nostre torn, busquem maximitzar
     else{
-      int[] min = new int[]{-1,0};
+      int[] min = new int[]{(int)(t.getMida() * Math.random()),9999999};
       for (int i = 0; i < t.getMida(); i++){
         if (t.movpossible(i)){
           // Creem un nou tauler per a realitzar el moviment
@@ -268,16 +263,16 @@ public class MinMax implements Jugador, IAuto{
           copia.afegeix(i, color);
 
           // Recursivitat: cridem a la funcio amb el nou tauler, el color contrari i la profunditat disminuida, i actualitzem el valor de beta
-          int[] val = minimax(copia, -1, profunditat - 1, alpha, beta);
+          int[] proxim = minimax(copia, -1, profunditat - 1, alpha, beta, i);
           
-          if(val[0] == -1 || val[1] < min[1]){
+          if(proxim[1] < min[1]){
             min[0] = i;
-            min[1] = heuristica(t);
-            beta = heuristica(t);
+            min[1] = proxim[1];
           }
+          beta = Math.min(beta, proxim[1]);
 
           // Si el valor de beta es menor o igual que alpha, retornem el valor de beta
-          if (beta <= alpha) return min;
+          if (beta <= alpha) break;
         }
       }
       return min;
@@ -285,32 +280,27 @@ public class MinMax implements Jugador, IAuto{
   }
 
   public int moviment(Tauler t, int color)
-  {   
-    int[] a = minimax(t, 1, 8, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  { 
     
-    // Si no hi h cap moviment bo, fem un moviment aleatori
-    if (a[0] == -1){
-      System.out.println("MOVIMENT ALERATORI");
+    boolean buit = true;
+    int i = 0;
+    while(buit && i < t.getMida())
+    {
+      if (t.getColor(0, i) != 0 || t.getColor(0, i) == color)
+      {
+        buit = false;
+      }
+      ++i;
+    }
+    if (buit) {
+      System.out.println("RANDOM");
       return (int)(t.getMida() * Math.random());
     }
-    System.out.println("MILLOR HEURISTICA: "+a[0]+" - COLUMNA: "+a[1]);
-    return a[0];
 
-    /*
-    for(int i = 0; i < t.getMida(); ++i)
-    {
-      if(t.movpossible(i)){
-        
-        if(a > heuristica)
-        {
-          col = i;
-          heuristica = a;
-        }
-      }
-    }
-    */
-    
-    
+    int[] a = minimax(t, color, 8, -9999999, 9999999, 0);
+
+    System.out.println("MILLOR HEURISTICA: "+a[1]+" - COLUMNA: "+a[0]);
+    return a[0];    
   }
 
   public String nom()
